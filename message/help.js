ed02574366c17e3fa410404e2d1add6125cd4388
xdlyy404 <subsxdlyy@gmail.com>
2022-02-01 06:45:21 +0100

    commit by jayo

---
+++ message/help.js
@@ -0,0 +1,65 @@
+exports.menu = (ucapan, pushname, ownerName, botName, tanggal, jam, runtime, prefix) => {
+    return `${ucapan.data.result} ${pushname}
+
+        『 *OWNER & BOT INFO* 』
+👑 Creator : ${ownerName}
+👸 Pacar Owner : Fafa Kawaii
+🤖 Bot Name : ${botName}
+📆 Tanggal : ${tanggal}
+⌚ Jam : ${jam}
+⏳ Runtime
+${runtime}
+
+        『 *TOOLS MENU* 』
+き⃟🐣 ${prefix}sticker
+き⃟🐣 ${prefix}attp
+き⃟🐣 ${prefix}nulis
+
+        『 *GROUP MENU* 』
+き⃟🐣 ${prefix}add
+き⃟🐣 ${prefix}kick
+き⃟🐣 ${prefix}welcome
+き⃟🐣 ${prefix}left
+き⃟🐣 ${prefix}setwelcome
+き⃟🐣 ${prefix}changewelcome
+き⃟🐣 ${prefix}delsetwelcome
+き⃟🐣 ${prefix}setleft
+き⃟🐣 ${prefix}changeleft
+き⃟🐣 ${prefix}delsetleft
+き⃟🐣 ${prefix}mute
+き⃟🐣 ${prefix}hidetag
+き⃟🐣 ${prefix}opengrup
+き⃟🐣 ${prefix}closegrup
+き⃟🐣 ${prefix}antilink
+き⃟🐣 ${prefix}antiwame
+き⃟🐣 ${prefix}checksewa
+き⃟🐣 ${prefix}setcmd
+き⃟🐣 ${prefix}delcmd
+
+        『 *DOWNLOAD MENU* 』
+き⃟🐣 ${prefix}instagram <error>
+き⃟🐣 ${prefix}youtube <error>
+き⃟🐣 ${prefix}tiktok
+
+        『 *STORE MENU* 』
+き⃟🐣 ${prefix}addlist
+き⃟🐣 ${prefix}dellist
+き⃟🐣 ${prefix}update
+き⃟🐣 ${prefix}list
+き⃟🐣 y < reply orderan >
+き⃟🐣 d < reply orderan >
+
+        『 *OWNER MENU* 』
+き⃟🐣 ${prefix}bc
+き⃟🐣 ${prefix}setlogo
+き⃟🐣 ${prefix}setprefix
+き⃟🐣 ${prefix}exif
+き⃟🐣 ${prefix}public
+き⃟🐣 ${prefix}self
+き⃟🐣 ${prefix}ban
+き⃟🐣 ${prefix}unban
+き⃟🐣 ${prefix}join
+き⃟🐣 ${prefix}addsewa
+き⃟🐣 ${prefix}delsewa
+`
+}
